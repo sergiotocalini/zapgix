7c8d766544edd502e9800ac7bc375ec78dcc6ce2
Sergio Tocalini Joerg <stocal1@Sergios-MacBook-Pro.local>
2018-02-12 09:54:25 +0100

    add zabbix install script

---
+++ deploy_zabbix.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env ksh
+SOURCE_DIR=$(dirname $0)
+ZABBIX_DIR=/etc/zabbix
+
+mkdir -p ${ZABBIX_DIR}/scripts/agentd/zapgix
+cp -r ${SOURCE_DIR}/zapgix/sql ${ZABBIX_DIR}/scripts/agentd/zapgix/
+cp ${SOURCE_DIR}/zapgix/zapgix.conf.example ${ZABBIX_DIR}/scripts/agentd/zapgix/
+cp ${SOURCE_DIR}/zapgix/zapgix.sh ${ZABBIX_DIR}/scripts/agentd/zapgix/
+cp ${SOURCE_DIR}/zapgix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/
