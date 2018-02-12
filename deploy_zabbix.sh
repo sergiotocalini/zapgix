fabd787f5df7e978d25b10d44b0994427989a7d4
Sergio Tocalini Joerg <stocal1@Sergios-MacBook-Pro.local>
2018-02-12 09:56:02 +0100

    fix permissions

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -6,4 +6,4 @@ mkdir -p ${ZABBIX_DIR}/scripts/agentd/zapgix
 cp -r ${SOURCE_DIR}/zapgix/sql ${ZABBIX_DIR}/scripts/agentd/zapgix/
 cp ${SOURCE_DIR}/zapgix/zapgix.conf.example ${ZABBIX_DIR}/scripts/agentd/zapgix/
 cp ${SOURCE_DIR}/zapgix/zapgix.sh ${ZABBIX_DIR}/scripts/agentd/zapgix/
-cp ${SOURCE_DIR}/zapgix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/
+cp ${SOURCE_DIR}/zapgix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/zapgix.conf
