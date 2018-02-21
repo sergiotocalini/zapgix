5cbcc1cbe8632275041abad06a7bb0c12b64ec80
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-02-21 15:58:56 +0100

    Update deploy_zabbix.sh

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -4,6 +4,5 @@ ZABBIX_DIR=/etc/zabbix
 
 mkdir -p ${ZABBIX_DIR}/scripts/agentd/zapgix
 cp -r ${SOURCE_DIR}/zapgix/sql ${ZABBIX_DIR}/scripts/agentd/zapgix/
-cp ${SOURCE_DIR}/zapgix/zapgix.conf.example ${ZABBIX_DIR}/scripts/agentd/zapgix/
 cp ${SOURCE_DIR}/zapgix/zapgix.sh ${ZABBIX_DIR}/scripts/agentd/zapgix/
 cp ${SOURCE_DIR}/zapgix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/zapgix.conf
